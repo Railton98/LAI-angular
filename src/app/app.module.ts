bb4d9c809026261eef803604b22cf8f3f04944d0
Rayltton's Teck <railtonleal98@gmail.com>
2018-04-21 00:09:35 -0300

    Adicionando ao Versionamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AlbumListComponent } from './album-list/album-list.component';
+import { AlbumCreateComponent } from './album-create/album-create.component';
+import { PhotoCreateComponent } from './photo-create/photo-create.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+const routes:Routes = [
+  {path:'albums', component: AlbumListComponent},
+  {path:'albums/create', component: AlbumCreateComponent},
+  {path:'albums/:album/photos/create', component: PhotoCreateComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumListComponent,
+    AlbumCreateComponent,
+    PhotoCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash: true}),
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/photo-create/photo-create.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-photo-create',
+  templateUrl: './photo-create.component.html',
+  styleUrls: ['./photo-create.component.css']
+})
+export class PhotoCreateComponent implements OnInit {
+
+  name = '';
+  album = null;
+  file = null;
+
+  constructor(private http:HttpClient, private route:ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.getAlbum(params['album'])
+    })
+  }
+
+  getAlbum(albumId){
+    this.http.get(`http://localhost:8000/api/albums/${albumId}`)
+      .subscribe(data => this.album = data);
+  }
+
+  handleFile(file){
+    this.file = file;
+  }
+
+  upload(){
+    const formData = new FormData();
+    formData.append('name', this.name);
+    formData.append('file_name', this.file);
+
+    this.http.post(`http://localhost:8000/api/albums/${this.album.id}/photos`, formData)
+      .subscribe(data => alert('Upload Realizado!'));
+  }
+
+}
